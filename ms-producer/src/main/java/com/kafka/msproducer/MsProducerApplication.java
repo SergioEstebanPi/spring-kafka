508ce79524bd4b54660166c6ea65d8b5547c6926
Sergio Piña <sepi_147@hotmail.com>
2023-12-05 16:01:01 -0500

    Envio mensaje al topic desde el productor al iniciar el ms

--- ms-producer/src/main/java/com/kafka/msproducer/MsProducerApplication.java
+++ ms-producer/src/main/java/com/kafka/msproducer/MsProducerApplication.java
@@ -1,13 +1,21 @@
 package com.kafka.msproducer;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class MsProducerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsProducerApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(KafkaTemplate<String, String> kafkaTemplate){
+		return args -> kafkaTemplate.send("example-topic-1", "Hello world from SpringKafka");
 	}
 
 }
